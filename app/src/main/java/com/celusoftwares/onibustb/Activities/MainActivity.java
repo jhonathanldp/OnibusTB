7532ef2d77cd05f5b9e5fc3a335310cf328422f4
jhonathanldp <jhonathanldp@outlook.com>
2016-12-27 22:09:32 -0200

    Criado a interface da tela principal

---
+++ app/src/main/java/com/celusoftwares/onibustb/Activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.celusoftwares.onibustb.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.celusoftwares.onibustb.R;
+
+/**
+ * Created by jhona on 27/12/2016.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_principal);
+    }
+}
