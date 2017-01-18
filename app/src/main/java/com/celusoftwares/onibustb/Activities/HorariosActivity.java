413da79647faf9d33aa94ddf1c64bda0af8ec58a
jhonathanldp <jhonathanldp@outlook.com>
2017-01-18 00:43:02 -0200

    Adicionado suporte a favoritos e multithreading a tela horarios

--- app/src/main/java/com/celusoftwares/onibustb/Activities/HorariosActivity.java
+++ app/src/main/java/com/celusoftwares/onibustb/Activities/HorariosActivity.java
@@ -1,26 +1,25 @@
 package com.celusoftwares.onibustb.Activities;
 
-import android.app.Activity;
-import android.content.Context;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.celusoftwares.onibustb.R;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
+import Background.CarregadorDeHorarios;
 import BancoDeDados.DataBase;
 import Models.Horario;
 import adapter.AdapterHorarios;
@@ -29,90 +28,99 @@ import adapter.AdapterHorarios;
  * Created by jhona on 08/01/2017.
  */
 
-public class HorariosActivity extends Activity implements AdapterView.OnItemClickListener {
+public class HorariosActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private DataBase dataBase;
     private AdapterHorarios adapterHorarios;
     private List<Horario> horarioList;
     private ListView listView;
     private Spinner spinner;
+    private AlertDialog.Builder alertDialog;
+    private int idHorario;
+    private ProgressBar progressBar;
+    private CarregadorDeHorarios carregadorDeHorarios;
+    private ContentValues contentValues;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.horarios);
 
-        Log.println(Log.INFO, "teste", "teste");
+        setTitle(R.string.nome_titulo);
+
+        alertDialog = new AlertDialog.Builder(this);
 
         dataBase = new DataBase(this);
         listView = (ListView) findViewById(R.id.lista_horarios);
         spinner = (Spinner) findViewById(R.id.selectTipoHorario);
 
-        /*File dataChecker = getApplicationContext().getDatabasePath(dataBase.DBNOME);
-
-        if(!dataChecker.exists()){
-            dataBase.getReadableDatabase();
-
-            if(copyDatabase(this)){
-                Toast.makeText(this, "Banco copiado", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                Toast.makeText(this, "Hmm, ocoreu um erro :(", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        }*/
-
-        ArrayAdapter<CharSequence> arrayAdapter =  ArrayAdapter.createFromResource(this, R.array.selecaoTipoHorario, R.layout.list_item_spinner);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.selecaoTipoHorario, R.layout.list_item_spinner);
         arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
 
         spinner.setAdapter(arrayAdapter);
-        String[] parametro = new String[]{getIntent().getStringExtra("id_regiao")};
 
-        horarioList = dataBase.listarHorarios(parametro);
-        Log.println(Log.INFO, "teste", String.valueOf(horarioList.size()));
+        contentValues = new ContentValues();
+
+        progressBar = (ProgressBar) findViewById(R.id.barraProgressoHorario);
+
+        carregadorDeHorarios = new CarregadorDeHorarios(progressBar, dataBase, this, this);
+
+        String favoritoPress = getIntent().getStringExtra("favorito");
+
+        if ("true".equals(favoritoPress)) {
+            contentValues.put("parametro", "1");
+            contentValues.put("collum", "favorito");
+        } else {
+            contentValues.put("parametro", getIntent().getStringExtra("id_regiao"));
+            contentValues.put("collum", "id_regiao");
+        }
+
+        carregadorDeHorarios.execute(contentValues);
+
+    }
+
+    public void callbackCarregarHorarios(List<Horario> result) {
+        horarioList = result;
         adapterHorarios = new AdapterHorarios(this, horarioList);
 
-        listView.setAdapter(adapterHorarios);
-        Log.println(Log.INFO, "teste", "teste3");
+        if (listView.getCount() != 0) {
+            adapterHorarios.notifyDataSetChanged();
+            listView.setAdapter(adapterHorarios);
+        } else {
+            listView.setAdapter(adapterHorarios);
 
+            listView.setOnItemClickListener(this);
+        }
+
+    }
+
+    public void adicionarFavorito(View view) {
+        if (dataBase.adicionarFavoritoDAO(idHorario)) {
+            Toast.makeText(this, "Esse Horario acaba de ser adicionado aos Favoritos!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Esse Horario acaba de ser removido dos Favoritos!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
-   /* private boolean copyDatabase(Context context){
-        try {
-            InputStream inputStream = context.getAssets().open(dataBase.DBNOME);
-            String outFileName = dataBase.DBLOCATION + dataBase.DBNOME;
-            OutputStream outputStream = new FileOutputStream(outFileName);
-            byte[] buff = new byte[1024];
-            int tamanho = 0;
-
-            while ((tamanho = inputStream.read(buff)) > 0){
-                outputStream.write(buff,0, tamanho);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        idHorario = horarioList.get(position).getId_horario();
+        alertDialog.setView(layoutInflater.inflate(R.layout.dialog_favorito, parent, false));
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                recarregarItens();
+                dialog.dismiss();
             }
-
-            outputStream.flush();
-            outputStream.close();
-
-            return true;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(context, "Isso é constrangedor, mas encontramos um problema :(", Toast.LENGTH_SHORT).show();
-            return false;
-        }
+        });
+        alertDialog.create();
+        alertDialog.show();
+        Log.v("Teste", "DialogoFav list click");
     }
 
-    public void favoritoChecked(View v){
-        boolean checked = ((CheckBox)v).isChecked();
-
-        if (checked){
-            ((CheckBox) v).setBackgroundResource(R.drawable.ic_star_black_24dp);
-        }
-        else if (!checked){
-            ((CheckBox) v).setBackgroundResource(R.drawable.ic_star_border_black_24dp);
-        }
-    }*/
+    public void recarregarItens() {
+        carregadorDeHorarios = new CarregadorDeHorarios(progressBar, dataBase, this, this);
+        carregadorDeHorarios.execute(contentValues);
+    }
 }
